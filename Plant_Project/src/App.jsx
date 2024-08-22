9f59b3a8f15306fb669e450fe958dc59187a9812
Rahul Yadav <rahul_203022@saitm.org>
2024-08-22 16:47:37 +0530

    plant

---
+++ Plant_Project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <>
+  <div className="bg-black text-white">hello world</div>
+  </>
+  )
+}
+
+export default App
